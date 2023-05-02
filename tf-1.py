1a1d6a300b09997d6b9ea57761cc686c8b4daca0
Alireza Saei <saeialireza359@gmail.com>
2023-05-02 16:00:42 +0430

    Input numpy arrays

--- tf-1.py
+++ tf-1.py
@@ -1,13 +1,15 @@
-import tensorflow
-import keras
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 # Simple NN with 1 neuron
 model = keras.Sequential([
     keras.layers.Dense(units=1 ,input_shape=[1])
 ])
-
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-model.fit([-1, 0, 1, 2, 3, 4, 5], [-3, -1, 1, 3, 5, 7, 9], epochs=50)
+x = np.array([-1, 0, 1, 2, 3, 4, 5], dtype=int)
+y = np.array([-3, -1, 1, 3, 5, 7, 9], dtype=int)
+model.fit(x, y, epochs=50)
 
 print(model.predict([10]))
