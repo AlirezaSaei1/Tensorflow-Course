f8278072481af82f86ec088635570152f18eb043
Alireza Saei <saeialireza359@gmail.com>
2023-06-05 00:41:19 +0430

    Sequential Model code for the dataset

--- tf-2.py
+++ tf-2.py
@@ -5,8 +5,14 @@ from tensorflow import keras
 fmnist = tf.keras.datasets.fashion_mnist
 (x_train, y_train), (x_test, y_test) = fmnist.load_data()
 
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)    
 ])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+model.fit(x_train, y_train, epochs=5)
