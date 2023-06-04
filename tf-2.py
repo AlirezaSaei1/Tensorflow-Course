1919a57f3650631a4eb0f42882b2275211d3fd67
Alireza Saei <saeialireza359@gmail.com>
2023-06-05 00:46:43 +0430

    Added callback to stop trainig at specific loss amount

--- tf-2.py
+++ tf-2.py
@@ -2,11 +2,20 @@ import tensorflow as tf
 from tensorflow import keras
 
 
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        if (logs.get('loss') < 0.4):
+            print('\nLoss is acceptable! Cancelling training...')
+            self.model.stop_training = True
+
+
 fmnist = tf.keras.datasets.fashion_mnist
 (x_train, y_train), (x_test, y_test) = fmnist.load_data()
 
 x_train = x_train / 255.0
 x_test = x_test / 255.0
+
+callback = myCallback()
 
 model = keras.Sequential([
     keras.layers.Flatten(),
@@ -15,4 +24,4 @@ model = keras.Sequential([
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=5, callbacks=[callback])
