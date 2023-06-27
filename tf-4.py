eb18351f6a9b32a8df17d855d467666997b94061
Alireza Saei <saeialireza359@gmail.com>
2023-06-28 00:04:02 +0430

    added model and compilation

---
+++ tf-4.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+
+train_dir = ''
+train_data_gen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_data_gen.flow_from_directory(
+    train_dir,
+    target_size = (300, 300),
+    batch_size = 128,
+    class_mode = 'binary'
+)
+
+valid_dir = ''
+valid_data_gen = ImageDataGenerator(rescale=1./255)
+
+valid_generator = valid_data_gen.flow_from_directory(
+    valid_dir,
+    target_size = (300, 300),
+    batch_size = 32,
+    class_mode = 'binary'
+)
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu',
+    input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(loss='binary_crossentropy',
+              optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.01),
+              metrics=['accuracy'])
