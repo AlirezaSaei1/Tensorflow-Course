fdbb7872aba6899829a31a1a85f7c6c2a6872804
Alireza Saei <saeialireza359@gmail.com>
2023-07-11 15:57:51 +0430

    Added text to sequence

--- tf3-6.py
+++ tf3-6.py
@@ -6,10 +6,16 @@ sentences = [
     'I love my dog',
     'I love my cat',
     # Punctuation marks are stripped
-    'You love my dog!'
+    'You love my dog!',
+    # Sentence with different length
+    'Do you think my dog is amazing?'
 ]
 
+# When you don't know the number of words this will take the most frequent ones
 tokenizer = Tokenizer(num_words=100)
 tokenizer.fit_on_texts(sentences)
 word_idx = tokenizer.word_index
 print(word_idx)
+
+sequences = tokenizer.texts_to_sequences(sentences)
+print(sequences)
