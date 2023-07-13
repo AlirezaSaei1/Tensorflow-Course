0d32e7d295782add8567567b472fdf2e61466e3a
Alireza Saei <saeialireza359@gmail.com>
2023-07-13 20:01:36 +0430

    Added tokenizer

--- tf3-3.py
+++ tf3-3.py
@@ -1,26 +1,51 @@
 # Embedding
 import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow_datasets as tfds
+import numpy as np
+
 print(tf.__version__)
 
-import tensorflow_datasets as tfds
+# Show list of datasets
 tfds.list_builders()
+
+# Load imdb_reviews datasets
 imdb, info = tfds.load('imdb_reviews', with_info=True, as_supervised=True)
 
-import numpy as np
 train_data, test_data = imdb['train'], imdb['test']
 
 training_sentences = []
-training_lables = []
+training_labels = []
 
 testing_sentences = []
-testing_lables = []
+testing_labels = []
 
 # Loop over training data and save sentences and labels
 for s, l in train_data:
     training_sentences.append(s.numpy().decode('utf8'))
-    training_lables.append(l.numpy())
+    training_labels.append(l.numpy())
 
 # Loop over testing data and save sentences and labels
 for s, l in test_data:
     testing_sentences.append(s.numpy().decode('utf8'))
-    testing_lables.append(l.numpy())
+    testing_labels.append(l.numpy())
+
+
+training_labels_final = np.array(training_labels)
+testing_labels_final = np.array(testing_labels)
+
+vocab_size = 10000
+embedding_dim = 16
+max_length = 120
+trunc_type='post'
+oov_tok = "<OOV>"
+
+
+tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(training_sentences)
+padded = pad_sequences(sequences,maxlen=max_length, truncating=trunc_type)
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences,maxlen=max_length)
