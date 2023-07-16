5bbc074d14764c1c5b5e39f39830bebac696b2d1
Alireza Saei <saeialireza359@gmail.com>
2023-07-16 15:40:30 +0430

    Output of LSTM and stacking them

--- My Codes/tf3-4.py
+++ My Codes/tf3-4.py
@@ -4,14 +4,18 @@ import tensorflow as tf
 # ....
 
 # tokenizer.vocab_size
-vocab_size = 0
+vocab_size = 64
 
 model = tf.keras.Sequential([
     tf.keras.layers.Embedding(vocab_size, 64),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    # LSTMs can be stacked like Dense layers
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
+    # Desired output is 64 but becuase it is bidirectional in model.sumamry it has 128 output
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
     tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
+model.summary()
 
 # ....
