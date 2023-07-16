7f7f49166a1f90207c88075d188ac58a8490d7b2
Alireza Saei <saeialireza359@gmail.com>
2023-07-16 15:43:36 +0430

    Completed code with LSTM

--- My Codes/tf3-4.py
+++ My Codes/tf3-4.py
@@ -1,21 +1,64 @@
 # This code will be about RNNs and LSTMs
+# Import required libraries
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+
+# Download the subword encoded pretokenized dataset
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True)
+
+# Get the tokenizer
+tokenizer = info.features['text'].encoder
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 256
+
+# Get the train and test splits
+train_data, test_data = dataset['train'], dataset['test'], 
+
+# Shuffle the training data
+train_dataset = train_data.shuffle(BUFFER_SIZE)
+
+# Batch and pad the datasets to the maximum length of the sequences
+train_dataset = train_dataset.padded_batch(BATCH_SIZE)
+test_dataset = test_data.padded_batch(BATCH_SIZE)
+
+
 import tensorflow as tf
 
-# ....
+# Hyperparameters
+embedding_dim = 64
+lstm_dim = 64
+dense_dim = 64
 
-# tokenizer.vocab_size
-vocab_size = 64
-
+# Build the model
 model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size, 64),
-    # LSTMs can be stacked like Dense layers
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
-    # Desired output is 64 but becuase it is bidirectional in model.sumamry it has 128 output
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
-    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Embedding(tokenizer.vocab_size, embedding_dim),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_dim)),
+    tf.keras.layers.Dense(dense_dim, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
+# Print the model summary
 model.summary()
 
-# ....
+# Set the training parameters
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+NUM_EPOCHS = 10
+history = model.fit(train_dataset, epochs=NUM_EPOCHS, validation_data=test_dataset)
+
+
+# Plot utility
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+
+# Plot the accuracy and results 
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
