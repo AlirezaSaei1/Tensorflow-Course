c0125aa99be6e1d0f049ac2581f1fad7d219980a
Alireza Saei <saeialireza359@gmail.com>
2023-07-16 15:37:29 +0430

    Added code for NN with LSTM

--- My Codes/tf3-4.py
+++ My Codes/tf3-4.py
@@ -1,1 +1,17 @@
 # This code will be about RNNs and LSTMs
+import tensorflow as tf
+
+# ....
+
+# tokenizer.vocab_size
+vocab_size = 0
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, 64),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+
+# ....
