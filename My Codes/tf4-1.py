8829502a5f3e25c3211cc730087dae2d667d2aa6
Alireza Saei <saeialireza359@gmail.com>
2023-07-25 17:53:40 +0430

    Added function to plot series

--- My Codes/tf4-1.py
+++ My Codes/tf4-1.py
@@ -1,1 +1,25 @@
 # Time-series
+
+# Required libraries
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+# Function used for plotting
+def plot_series(time, series, format='-', start=0, end=None):
+    plt.figure(figsize=(10, 6))
+
+    if type(series) is tuple:
+        for i in series:
+            plt.plot(time[start:end], i[start:end], format)
+    else:
+        plt.plot(time[start:end], series[start:end], format)
+
+    plt.xlabel('Time')
+    plt.ylabel('Value')
+    plt.grid(True)
+    plt.show()
+
+
+
