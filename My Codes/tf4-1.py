ced6fb8ee73e8099399b4463a713f0bdba2ff79f
Alireza Saei <saeialireza359@gmail.com>
2023-07-25 17:59:27 +0430

    Added some function to create time series

--- My Codes/tf4-1.py
+++ My Codes/tf4-1.py
@@ -22,4 +22,30 @@ def plot_series(time, series, format='-', start=0, end=None):
     plt.show()
 
 
+# Function to create trend
+def trend(time, slope=0):
+    series = slope * time
+    return series
 
+
+# Function to create seasonal pattern
+def seasonal_pattern(season_time):
+    data_pattern = np.where(season_time < 0.4,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.exp(3 * season_time))
+
+    return data_pattern
+
+
+# Function to add seasonality
+def seasonality(time, period, amplitude=1, phase=0):
+    season_time = ((time + phase) % period) / period
+    data_pattern = amplitude * seasonal_pattern(season_time)
+    return data_pattern
+
+
+# Function to create noise
+def noise(time, noise_level=1, seed=None);
+    rnd = np.random.RandomState(seed)
+    noise = rnd.randn(len(time)) * noise_level
+    return noise
