246c13f128f2a90f844afc3824f96b0d31a83bee
Alireza Saei <saeialireza359@gmail.com>
2023-07-30 11:27:08 +0430

    Added code for windowing

--- My Codes/tf4-2.py
+++ My Codes/tf4-2.py
@@ -7,3 +7,16 @@ import numpy as np
 
 # create dataset using Dataset.range
 dataset = tf.data.Dataset.range(10)
+for val in dataset:
+    print(val.numpy())
+
+
+# Create windows without fixed size
+windows = dataset.window(5, shift=1)
+
+# Create widnows with fixed size
+windows = dataset.window(5, shift=1, drop_remainder=True)
+for window in windows:
+    for val in window:
+        print(val.numpy(), end=" ")
+    print('')
