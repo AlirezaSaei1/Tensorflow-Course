8e2ef75c9043ff4adb9dcd30c1b9723124b35168
Alireza Saei <saeialireza359@gmail.com>
2023-08-05 01:20:26 +0430

    Added code for windowing dataset

--- My Codes/tf4-3.py
+++ My Codes/tf4-3.py
@@ -1,5 +1,40 @@
 import importlib
 import numpy as np
+import tensorflow as tf
+
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    """Generates dataset windows
+
+    Args:
+      series (array of float) - contains the values of the time series
+      window_size (int) - the number of time steps to include in the feature
+      batch_size (int) - the batch size
+      shuffle_buffer(int) - buffer size to use for the shuffle method
+
+    Returns:
+      dataset (TF Dataset) - TF Dataset containing time windows
+    """
+  
+    # Generate a TF Dataset from the series values
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    
+    # Window the data but only take those with the specified size
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    
+    # Flatten the windows by putting its elements in a single batch
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+
+    # Create tuples with features and labels 
+    dataset = dataset.map(lambda window: (window[:-1], window[-1]))
+
+    # Shuffle the windows
+    dataset = dataset.shuffle(shuffle_buffer)
+    
+    # Create batches of windows
+    dataset = dataset.batch(batch_size).prefetch(1)
+    
+    return dataset
+
 tf4_1 = importlib.import_module("tf4-1")
 
 # Parameters
@@ -17,3 +52,22 @@ series += tf4_1.noise(time, noise_level, seed=42)
 
 # Plot the results
 tf4_1.plot_series(time, series)
+
+
+# Define the split time
+split_time = 1000
+
+# Get the train set 
+time_train = time[:split_time]
+x_train = series[:split_time]
+
+# Get the validation set
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+# Parameters
+window_size = 20
+batch_size = 32
+shuffle_buffer_size = 1000
+
+dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
