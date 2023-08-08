4f95c52563a9f093f5abaea0074270d39b6e1eca
Alireza Saei <saeialireza359@gmail.com>
2023-08-08 19:54:12 +0430

    plotting loss/lr code added

--- My Codes/tf4-3.py
+++ My Codes/tf4-3.py
@@ -1,6 +1,7 @@
 import importlib
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 tf4_1 = importlib.import_module("tf4-1")
 
@@ -70,5 +71,22 @@ model.compile(loss=tf.keras.losses.Huber(),
               optimizer=tf.keras.optimizers.SGD(learning_rate=1e-8, momentum=0.9),
               metrics=['mae'])
 
-x_train = []
 history = model.fit(x_train, epochs=19, callbacks=[lr_schedule])
+
+# Define the learning rate array
+lrs = 1e-8 * (10 ** (np.arange(100) / 20))
+
+# Set the figure size
+plt.figure(figsize=(10, 6))
+
+# Set the grid
+plt.grid(True)
+
+# Plot the loss in log scale
+plt.semilogx(lrs, history.history["loss"])
+
+# Increase the tickmarks size
+plt.tick_params('both', length=10, width=1, which='both')
+
+# Set the plot boundaries
+plt.axis([1e-8, 1e-3, 0, 30])
