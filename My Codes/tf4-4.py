613e48b943ebef8875c3629c4c06d0efa12470dd
Alireza Saei <saeialireza359@gmail.com>
2023-08-12 00:01:54 +0430

    Convert data to numpy arrays

--- My Codes/tf4-4.py
+++ My Codes/tf4-4.py
@@ -1,11 +1,19 @@
 import csv
+import numpy as np
 
 time_step = []
 sunspots = []
 
+# Read dataset
 with open('TimeSeries/Sunspots.csv') as f:
     reader = csv.reader(f, delimiter=',')
     next(reader)
     for row in reader:
         sunspots.append(float(row[2]))
         time_step.append(int(row[0]))
+
+
+# Convert lists to numpy arrays - everytime we append sth to numpy array lots of memory management for cloning the list
+# so it is better to first create a throwaway list then convert it to numpy arrays
+series = np.array(sunspots)
+time = np.array(time_step)
