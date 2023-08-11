e9d69117245069055fea7e232c4ff40ba9cd4781
Alireza Saei <saeialireza359@gmail.com>
2023-08-11 23:38:44 +0430

    Added NN with Conv1D

--- My Codes/tf4-3.py
+++ My Codes/tf4-3.py
@@ -57,7 +57,20 @@ model = tf.keras.models.Sequential([
 model_tune = tf.keras.models.Sequential([
   tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1),
                       input_shape=[window_size]),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, return_sequences=True)),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, return_sequences=True)),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+  tf.keras.layers.Dense(1),
+  tf.keras.layers.Lambda(lambda x: x * 100.0)
+])
+# ------------------------------------------------------------------------------------------------
+
+# Model with Conv1D
+# ------------------------------------------------------------------------------------------------
+model_tune = tf.keras.models.Sequential([
+  tf.keras.layers.Conv1D(filters=32, kernel_size=5, strides=1, 
+                          padding='casual', activation='relu', 
+                         input_shape=[None, 1]),
+  tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, return_sequences=True)),
   tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
   tf.keras.layers.Dense(1),
   tf.keras.layers.Lambda(lambda x: x * 100.0)
