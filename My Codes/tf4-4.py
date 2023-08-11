fb0913062ea0af6058c5379fa436829db64029f7
Alireza Saei <saeialireza359@gmail.com>
2023-08-12 00:15:19 +0430

    Added window_dataset from scratch

--- My Codes/tf4-4.py
+++ My Codes/tf4-4.py
@@ -1,6 +1,16 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda w: w.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer).map(lambda w: (w[:-1], w[-1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
+
 time_step = []
 sunspots = []
 
